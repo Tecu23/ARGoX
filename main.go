f834ed1645ef33bf6c3c86e9f133b9371782cc19
tecu23 <andrei.teculescu@gmail.com>
2024-03-26 10:48:37 +0200

    add function to generate magic numbers

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 // Package main contains the startup function and logic of the engine
 package main
 
-import "fmt"
-
 func main() {
 	InitPawnAttacks()
 	InitKnightAttacks()
 	InitKingAttacks()
 	InitFen2Sq()
 
-	for sq := A1; sq <= H8; sq++ {
-		b := generateBishopAttacks(sq)
-		fmt.Printf(" %d", b.Count())
-	}
+	InitMagic()
+
+	// for sq := A1; sq <= H8; sq++ {
+	// 	fmt.Printf(" %d", RookMagic)
+	// }
 }
