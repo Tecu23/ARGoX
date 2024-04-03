b725cabd42befbac026051202671462c20c5a878
tecu23 <andrei.teculescu@gmail.com>
2024-04-03 11:57:42 +0300

    add evaluate material + piece position

--- evaluate.go
+++ evaluate.go
@@ -1,1 +1,50 @@
 package main
+
+// EvaluatePosition should evaluate a certain position
+func (b *BoardStruct) EvaluatePosition() int {
+	score := 0
+
+	bb := Bitboard(0)
+
+	pc, sq := 0, 0
+
+	for bbPc := WP; bbPc <= BK; bbPc++ {
+		bb = b.Bitboards[bbPc]
+
+		for bb != 0 {
+			pc = bbPc
+
+			sq = bb.FirstOne()
+
+			score += MaterialScore[pc]
+
+			switch pc {
+			// evaluare white pieces
+			case WP:
+				score += PawnScore[sq]
+			case WN:
+				score += KnightScore[sq]
+			case WB:
+				score += BishopScore[sq]
+			case WR:
+				score += RookScore[sq]
+			case WK:
+				score += KingScore[sq]
+
+			// evaluate black pieces
+			case BP:
+				score -= PawnScore[MirrorScore[sq]]
+			case BN:
+				score -= KnightScore[MirrorScore[sq]]
+			case BB:
+				score -= BishopScore[MirrorScore[sq]]
+			case BR:
+				score -= RookScore[MirrorScore[sq]]
+			case BK:
+				score -= KingScore[MirrorScore[sq]]
+			}
+		}
+	}
+
+	return score
+}
--- main.go
+++ main.go
@@ -14,9 +14,21 @@ func main() {
 	GenerateSliderPiecesAttacks(Bishop) // bishop
 	GenerateSliderPiecesAttacks(Rook)   // rook
 
-	fmt.Println("Starting ARGoX")
+	InitMaterialScore()
 
-	Uci(input())
+	debug := true
 
-	fmt.Println("Quiting ARGoX")
+	if debug {
+		board := BoardStruct{}
+
+		ParseFEN(&board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ")
+
+		board.PrintBoard()
+
+		fmt.Printf("Score: %d\n", board.EvaluatePosition())
+	} else {
+		fmt.Println("Starting ARGoX")
+		Uci(input())
+		fmt.Println("Quiting ARGoX")
+	}
 }
